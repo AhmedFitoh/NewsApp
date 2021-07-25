1681f88ae6ff369751ec3218b25238985e735c20
AhmedFitoh <ahmed.fitoh.zakrya@protonmail.com>
2021-07-25 07:31:24 +0200

    Fix: - Add missing model.

--- NewsApp/Scenes/Home/HeadLinesScreen/Model/HeadLineModel.swift
+++ NewsApp/Scenes/Home/HeadLinesScreen/Model/HeadLineModel.swift
@@ -7,33 +7,7 @@
 
 import Foundation
 
-
-// MARK: - HeadLineModel
-struct HeadLineModel: Codable {
-    let status: String?
-    let totalResults: Int?
+//// MARK: - HeadLineModel
+struct HeadLineModel: Decodable {
     var articles: [Article]?
-}
-
-// MARK: - Article
-struct Article: Codable {
-    let source: Source?
-    let author: String?
-    let title: String?
-    let articleDescription: String?
-    let url: String?
-    let urlToImage: String?
-    let publishedAt, content: String?
-
-    enum CodingKeys: String, CodingKey {
-        case source, author, title
-        case articleDescription = "description"
-        case url, urlToImage, publishedAt, content
-    }
-}
-
-// MARK: - Source
-struct Source: Codable {
-    let id: String?
-    let name: String?
 }
